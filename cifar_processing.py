fab365b8dc1143458a62fba4f83211070d020e32
javromsev <56070548+javromsev@users.noreply.github.com>
2020-03-12 12:53:35 +0100

    Create cifar_processing.py

---
+++ cifar_processing.py
@@ -0,0 +1,5 @@
+## This class is aimed to preprocess CIFAR10/CIFAR100 dataset (available on Keras API) in order to convert all training sets 
+## from Tensors to Array
+
+def create_cifar_array():
+
