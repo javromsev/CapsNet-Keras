93a15ffe424e571cb929f358b7cb60ee818a0f6c
javromsev <56070548+javromsev@users.noreply.github.com>
2020-03-30 13:09:43 +0200

    Update capsulenet_fashion.py

--- capsulenet_fashion.py
+++ capsulenet_fashion.py
@@ -189,7 +189,7 @@ def manipulate_latent(model, data, args):
 
 def load_fashion_mnist():
     # the data, shuffled and split between train and test sets
-    from keras.datasets import fashion_mnist
+    from tensorflow.keras.datasets import fashion_mnist
     (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
     x_train = x_train.reshape(-1, 28, 28, 1).astype('float32') / 255.
